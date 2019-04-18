0487d2c79505c4ae46a81f1caf98e7449ee3963b
Ann <ann@matcher.tw>
2019-04-18 19:02:04 +0800

    增加template

--- js/main.js
+++ js/main.js
@@ -1,55 +1,102 @@
-// init setting
+// Start init setting
 var project = {
-    'id': 0,
-    'name': null,
+    'pid': 0,
+    'title': null,
     'endtime': null,
     'updatetime': null,
-    'states': null,
+    'states': null
 };
 
+var task = {
+    'tid': 0,
+    'pid': 0,
+    'title': null,
+    'endtime': null,
+    'updatetime': null,
+    'comment': [],
+    'tag': [],
+    'assign': null,
+    'states': null
+};
+
+var tag = {
+    'tid': 0,
+    'name': null,
+    'color': null
+}
+
+// Start template
+// 專案修改模板
+function getProjectInputTemplate(title) {
+    var template = `<ul class="project_ul"><li class="test">` + title + `</li></ul>`;
+    return template;
+}
+
 // get localStorage
-var localData = JSON.parse(localStorage.getItem("project")) || [];
+let localData = JSON.parse(localStorage.getItem("project")) || [];
 
 
 // Start 專案資料
 // 取得 新增專案資料
 function setInputProjectVal() {
-    var projectName = document.getElementById('project-name');
-    var projectEndtime = document.getElementById('project-endtime');
+    var projectName = document.querySelector('#project-name');
+    var projectEndtime = document.querySelector('#project-endtime');
 
-    if (projectName.value != '' || projectEndtime.value != '') {
+    if (projectName.value != '') {
         localData.push({
-            'id': localData.length + 1,
-            'name': projectName.value,
+            'pid': localData.length + 1,
+            'title': projectName.value,
             'endtime': projectEndtime.value,
             'updatetime': null,
             'states': null,
         })
         localStorage.setItem("project", JSON.stringify(localData));
 
-        updateProjectList();ZaSDSWER667
+        updateProjectList(localData.length);
     } else {
-        alert('請填寫名稱與日期');
+        alert('請填寫名稱');
     }
 }
 
-// 更新 專案列表
-function updateProjectList() {
-    var list = document.getElementById('project_list');
-    // 建立ul
-    var ul = document.createElement('ul');
-    ul.setAttribute("class", "project-ul");
-    var li;
+// update 
+function updateProjectList(len) {
+    let ul = document.querySelector('#project_list .project_ul');
+    let li = document.createElement('li');
 
-    localData.forEach((element, index) => {
-        li = document.createElement('li');
-        li.appendChild(document.createTextNode(element.name));
-        ul.appendChild(li);
-    });
-
-    document.getElementById('project_list').innerHTML(ul);
-
-    console.log(document.getElementById('project_list'));
+    ul.innerHTML = getProjectInputTemplate(localData[len-1].title);
+    // li.appendChild(document.createTextNode(localData[len-1].title));
+    // ul.appendChild(li);
 }
 
-updateProjectList();
+// loading 讀取專案列表
+function loadProjectList() {
+    let list = document.querySelector('#project_list');
+    // 建立ul
+    let ul = list.querySelector('.project_ul');
+    console.lo
+    // let li;
+
+    localData.forEach((element, index) => {
+        // li = document.createElement('li');
+        // li.appendChild(document.createTextNode(element.title));
+        // ul.appendChild(li);
+
+        getProjectInputTemplate(element.title);
+    });
+
+    // list.appendChild(ul);
+}
+
+// document ready
+const callback = function () {
+    loadProjectList();
+};
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    callback();
+} else {
+    document.addEventListener("DOMContentLoaded", callback);
+}
